93d30e51fb6c357b9892e19dcf42dcb879702eaa
BrookeKelly-coder <86648602+BrookeKelly-coder@users.noreply.github.com>
2021-10-19 20:00:38 +1000

    Crowdfunding App SetUp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route }from"react-router-dom";
+import "./App.css";
+import Nav from "./components/Nav/Nav";
+import HomePage from './pages/HomePage.jsx';
+import ProjectPage from './pages/ProjectPage.jsx';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <Router>
+        <div>
+        <Nav />
 
+        <Switch>
+            <Route path="/project">
+            <ProjectPage />
+            </Route>
+            <Route path="/">
+            <HomePage />
+            </Route>
+        </Switch>
+        </div>
+        </Router>
+        );
+    }
 export default App;
