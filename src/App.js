91eb2edcc333a8a443dd945830e9ae8b8f23926b
BrookeKelly-coder <86648602+BrookeKelly-coder@users.noreply.github.com>
2021-10-25 06:50:53 +1000

    Getting & Posting Data to the API

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import HomePage from './pages/HomePage.jsx';
 import ProjectPage from './pages/ProjectPage.jsx';
+import LoginPage from './pages/LoginPage';
 
 function App () {
   return (
         <Router>
         <div>
         <Nav />
-
         <Switch>
-            <Route path="/project">
+            <Route path="/project/:id">
             <ProjectPage />
+            </Route>
+            <Route path="/login">
+            <LoginPage />
             </Route>
             <Route path="/">
             <HomePage />
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "../components/LoginForm/LoginForm";
+
+function LoginPage() {
+    return (
+        <div>
+    <h1>This is the login page</h1>;
+    <LoginForm />
+    </div>)
+}
+
+export default LoginPage;
