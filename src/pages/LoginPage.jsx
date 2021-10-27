25ff4a8e417bda6bb199de866cf56e2d8f11cb10
BrookeKelly-coder <86648602+BrookeKelly-coder@users.noreply.github.com>
2021-10-27 13:11:57 +1000

    Updated Delete Button

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,7 +4,7 @@ import LoginForm from "../components/LoginForm/LoginForm";
 function LoginPage() {
     return (
         <div>
-    <h1>This is the login page</h1>;
+    <h1>This is the login page</h1>
     <LoginForm />
     </div>)
 }
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
+
 
 function ProjectPage(props) {
     const [projectData, setProjectData] = useState({pledges: [] });
     const { id } = useParams();
+    const history=useHistory()
     
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}projects/${id}`)
@@ -15,21 +17,33 @@ function ProjectPage(props) {
         });
     }, []);
     
+    const deleteProject = async () => {
+        await fetch(`${process.env.REACT_APP_API_URL}projects/${id}`, 
+        {   
+            method: "delete",
+            headers: {
+                "Authorization": `token ${window.localStorage.getItem("token")}`
+            }
+        })
+        history.push("/")
+    }
+
     return (
     <div>
-    <h2>{projectData.title}</h2>
-    <h3>Created at: {projectData.date_created}</h3>
-    <h3>{`Status: ${projectData.is_open}`}</h3>
-    <h3>Pledges:</h3>
-    <ul>
-    {projectData.pledges.map((pledgeData, key) =>{
-        return (
-        <li>
-        {pledgeData.amount} from {pledgeData.supporter}
-        </li>
-        );
-    })}
-    </ul>
+        <h2>{projectData.title}</h2>
+        <h3>Created at: {projectData.date_created}</h3>
+        <h3>{`Status: ${projectData.is_open}`}</h3>
+        <h3>Pledges:</h3>
+        <ul>
+            {projectData.pledges.map((pledgeData, key) =>{
+                return (
+                <li>
+                    {pledgeData.amount} from {pledgeData.supporter}
+                </li>
+                );
+            })}
+        </ul>
+        <button onClick={deleteProject}>Delete</button>
     </div>);
 }
 
