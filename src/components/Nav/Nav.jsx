a67ab02671fd1659d09b41506b738ef623fe9226
BrookeKelly-coder <86648602+BrookeKelly-coder@users.noreply.github.com>
2021-11-03 08:23:43 +1000

    Update Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// import React from "react";
+// import { Link } from "react-router-dom";
 
-function Nav() {
+// function Nav() {
+//     return (
+//         <nav>
+//         <Link to="/">Home</Link>
+//         </nav>
+//     );
+// }
+
+// export default Nav;
+
+const Nav = () => {
+    console.log('rendering nav bar')
+    const [loggedIn,setLoggedIn] = useState(localStorage.getItem('token'))
+
+    useEffect(()=>{
+        setLoggedIn(localStorage.getItem('token'))
+    })
+
+    const logout = () => {
+        setLoggedIn(false)
+        localStorage.clear()
+    }
+
     return (
-        <nav>
-        <Link to="/">Home</Link>
-        </nav>
-    );
-}
+         <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand>
+                    <Link to="/">Home</Link>
+                </Navbar.Brand>
 
-export default Nav;
+                <Link to="/Register">Register</Link>
+
+                {
+                loggedIn
+                ? <button onClick={logout}>Logout</button>
+                : <Link to="/login">Login</Link>
+                }
+
+            </Container>
+        </Navbar>   
+    )
+}
+ 
+export default Nav
